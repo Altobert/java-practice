81a0c97b4892ff1b41a510ae398c71ae58eb00f3
Altobert <alberto.sanmartin@usach.cl>
2023-08-15 13:43:25 -0400

    se agregan comentarios explicativos a ClassB.java

--- temp/packageb/ClassB.java
+++ temp/packageb/ClassB.java
@@ -1,11 +1,17 @@
-
+//carpeta se encuentra en packageb
 package packageb;
+
+//se importa ClassA
 import packagea.ClassA;
+
+
 public class ClassB{
 
- public static void main (String [] args){
-   ClassA a;
-   System.out.println("Got it");
- }    
+   //lo que hace el main es cambiar el estado del programa de 1 hacia cero. 
+   //quien tiene el metodo main debe ser la clase que coincida con el nombre del archivo.
+   public static void main (String [] args){
+     ClassA a = new ClassA();
+     System.out.println("Got it - esto es lo que se quiere lograr | ");
+   } 
 
 }
