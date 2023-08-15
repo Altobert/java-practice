6e679f9c43015f399d2aa464276170b07dde7726
Altobert <alberto.sanmartin@usach.cl>
2023-08-15 18:55:25 -0400

    comentarios

--- constructores/Chick.java
+++ constructores/Chick.java
@@ -1,6 +1,8 @@
 
 package constructores;
 
+//Ejemplo para demostrar el uso de constructores
+
 public class Chick{
 
 	//Metodo especial que devuelve objetos e inicializa variables.
@@ -10,6 +12,7 @@ public class Chick{
 
 	}
 
+	//Para poder realizar la prueba de construyo un main, de lo constrario no se podria ejecutar la clase
 	public static void main(String [] args){
 
 		//Se invoca al constructor al parecer :)
