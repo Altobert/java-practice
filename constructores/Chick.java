aeff2db8d3c2829eba505fe3de14070d76656e7d
Altobert <alberto.sanmartin@usach.cl>
2023-08-15 19:18:56 -0400

    se inicializan variables en un constructor

--- constructores/Chick.java
+++ constructores/Chick.java
@@ -5,12 +5,18 @@ package constructores;
 
 public class Chick{
 
-	//Metodo especial que devuelve objetos e inicializa variables.
+	//Metodo especial que crea objetos, es la funcion del constructor en java.
+	//Metodo crea un objeto del tipo Chick.
+	//Es n metodo que no retorna nada, pero que crea objetos.
+
+	//El proposito de un constructor es tambien inicializar campos o atributos
 	public Chick(){
 
 		System.out.println("dentro del constructor de la clase chic");
 
 	}
+
+	// public void Chick() { } // NOT A CONSTRUCTOR
 
 	//Para poder realizar la prueba de construyo un main, de lo constrario no se podria ejecutar la clase
 	public static void main(String [] args){
---
+++ constructores/Chicken.java
@@ -0,0 +1,28 @@
+
+
+package constructores;
+
+public class Chicken{
+
+	//atributo de clase es by default
+	int numberEggs=0;
+
+	//atributo de clase es by default
+	String name;
+
+	public Chicken(){
+		name="Ducke";
+	}
+
+	public static void main(String ... args){
+
+		System.out.println("Se inicializa objeto chicken para acceder a las variables");
+		Chicken chicken = new Chicken();
+		
+		//valor inicializado al momento de declarar el atributo de clase
+		System.out.println(" Numero huevos "+chicken.numberEggs);
+
+		//valor inicializado dentro del constructor
+		System.out.println(" Numero huevos "+chicken.name);
+	}
+}
