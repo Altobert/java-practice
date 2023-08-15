0d523d0702b9b86d3920e1794f09e95f0c17859c
Altobert <alberto.sanmartin@usach.cl>
2023-08-15 18:52:10 -0400

    practica constructores

---
+++ constructores/Chick.java
@@ -0,0 +1,20 @@
+
+package constructores;
+
+public class Chick{
+
+	//Metodo especial que devuelve objetos e inicializa variables.
+	public Chick(){
+
+		System.out.println("dentro del constructor de la clase chic");
+
+	}
+
+	public static void main(String [] args){
+
+		//Se invoca al constructor al parecer :)
+		new Chick();
+
+	}
+	
+}
