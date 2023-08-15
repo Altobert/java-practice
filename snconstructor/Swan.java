0c06bb5b33be25e3e9c6ab6249e2f981adc48296
Altobert <alberto.sanmartin@usach.cl>
2023-08-15 19:48:45 -0400

    se agrega string al final

--- snconstructor/Swan.java
+++ snconstructor/Swan.java
@@ -6,20 +6,23 @@ public class Swan{
 	//se inicializa en cero, al momemto de declarar la jvm le asigna el valor. 
 	int numberEggs;
 
-	//tambien se pueden leer y escribir variable directamente desde donde se declaran
-	String first = "Theodore";
+	//tambien se pueden leer y updatear variables directamente desde donde se declaran
+	//el signo + concatena
+	String first = "Theodore "; //se agrega otro String al final
 	String last = "Moose";
 	String full = first + last;
 
 	public static void main(String ... args){
 
-		//variable del tipo Swan, para almacenar la referencia del objeto
+		// variable del tipo Swan, para almacenar la referencia del objeto
 		// new, se esta inicializando y llamando al constructor Swan();
 		Swan mother = new Swan();
 
 		mother.numberEggs =1; // se realiza cambio del estado o seteo de un valor a la variable numberEggs
 
 		System.out.println(mother.numberEggs);
+
+		System.out.println(mother.full);
 
 	}
 
