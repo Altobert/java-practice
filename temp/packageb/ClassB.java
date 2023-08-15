d6de48b59dc732617c4ff380a5a97cef7ade6df3
Altobert <alberto.sanmartin@usach.cl>
2023-08-15 18:31:17 -0400

    se crea metodo de prueba

--- temp/packageb/ClassB.java
+++ temp/packageb/ClassB.java
@@ -3,6 +3,8 @@ package packageb;
 
 //se importa ClassA
 import packagea.ClassA;
+import java.util.ArrayList; 
+import java.util.List; 
 
 
 public class ClassB{
@@ -11,7 +13,29 @@ public class ClassB{
    //quien tiene el metodo main debe ser la clase que coincida con el nombre del archivo.
    public static void main (String [] args){
      ClassA a = new ClassA();
+     ClassB b = new ClassB();
+          
      System.out.println("Got it - esto es lo que se quiere lograr | ");
+
+     //Se inicializa objeto arraylist
+     ArrayList<String> myList = new ArrayList<String>(); 
+     //se agrega un elemento
+     myList.add("primer elemento");
+     //finalmente se entrega el objeto arraylist al metodo "method"
+     b.method(myList);
    } 
+
+
+   /**
+    *comentarios tipo java doc 
+    */
+   public void method(ArrayList list){
+
+      if(list.isEmpty()){
+          System.out.println("e");
+      }
+      System.out.println("n");
+
+   }
 
 }
