31004f2b0949bc393b144356f40d24ffeda19bfa
Altobert <alberto.sanmartin@usach.cl>
2023-08-15 23:28:35 -0400

    orden inicializacion

---
+++ orderinitialzise/InitSample.java
@@ -0,0 +1,13 @@
+
+package orderinitialzise;
+
+public class InitSample{
+
+	public static void main(String ... args){
+
+		{ System.out.println("feathers"); }
+
+	}
+	
+	{ System.out.println("Snowy");}
+}
