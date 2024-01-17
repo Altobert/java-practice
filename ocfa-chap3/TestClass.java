5be38800c556054cef8b90a4167dcccd1114f3a6
Alberto San Martin <alberto.sanmartin@sii.cl>
2024-01-17 15:45:23 -0300

    verificacion de creacion de objetos

--- ocfa-chap3/TestClass.java
+++ ocfa-chap3/TestClass.java
@@ -1,5 +1,11 @@
 public class TestClass{
 
+    private static int contador;
+    public TestClass(){
+        contador++;
+        System.out.println("Constructor TestClass: "+contador);
+    }
+
     public static void main(String[] args)throws Exception{
 
         //Salida no lansa excepcion por que es un metodo length maneja un dato primitivo no null
@@ -8,6 +14,10 @@ public class TestClass{
         for(int i=0;i<args.length;i++){
             System.out.println("args["+i+"]: "+args[i]);
         }
+
+        TestClass t1, t2, t3, t4; 
+        t1 = t2 = new TestClass();
+        t3 = new TestClass();
 
     }
 }
