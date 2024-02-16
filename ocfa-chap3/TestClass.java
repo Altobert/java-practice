8064add7556d4bc643ea02c0371288ec7350b7a3
Altobert <alberto.sanmartin@usach.cl>
2024-02-16 20:56:08 -0300

    practicas java

--- ocfa-chap3/TestClass.java
+++ ocfa-chap3/TestClass.java
@@ -9,16 +9,29 @@ public class TestClass{
 
     public static void main(String[] args)throws Exception{
 
-        //Salida no lansa excepcion por que es un metodo length maneja un dato primitivo no null
+        //Salida no lanza excepcion por que es un metodo length maneja un dato primitivo no null
         //si no que cero. 
         System.out.println("Largo argumentos de entrada: "+args.length);
+
         for(int i=0;i<args.length;i++){
             System.out.println("args["+i+"]: "+args[i]);
         }
 
         TestClass t1, t2, t3, t4; 
+        //ambos apuntan a la misma referencia
         t1 = t2 = new TestClass();
+        System.out.println("t1 "+t1.toString());
+        System.out.println("t2 "+t2.toString());
+        // se crea otra referencia.
         t3 = new TestClass();
+        System.out.println("t3 "+t3.toString());
 
     }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
+
 }
